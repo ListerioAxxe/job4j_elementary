e82d18dffb0fc84621c7b3c302df8c086e29b991
ListerioAxxe <langua095@gmail.com>
2021-01-08 11:28:59 +0300

    calculator

---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String [] args) {
+        int one = 1;
+        int two = 2;
+        int OnePlusTwo = one + two;
+        System.out.println(OnePlusTwo);
+        int six = 6;
+        int five = 5;
+        int four = 4;
+        int SixDivTwo = six / two;
+        System.out.println(SixDivTwo);
+        int FiveMinusTwo = five - two;
+        System.out.println(FiveMinusTwo);
+        int FiveTimeTwo = five * two;
+        System.out.println(FiveTimeTwo);
+    }
+}
