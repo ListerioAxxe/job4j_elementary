8a90dfbdaf4b3d8bccc6d1500a7da054a8ff600c
ListerioAxxe <langua095@gmail.com>
2021-01-09 20:11:41 +0300

    Greeting

---
+++ src/ru/job4j/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java";
+        System.out.println(idea);
+        String separate = " ";
+        String idea1 = "But i am a newbie";
+        String idea2 = idea + separate + idea1;
+        int year = 28;
+        String full = idea + separate + year;
+        System.out.println(full);
+
+    }
+}
