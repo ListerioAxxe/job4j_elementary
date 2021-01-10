bbe62f2b45c463cd4ff260ce3e3aebc485ba666e
ListerioAxxe <langua095@gmail.com>
2021-01-10 15:29:04 +0300

    converter

--- src/ru/job4j/coverter/Converter.java
+++ src/ru/job4j/coverter/Converter.java
@@ -2,17 +2,27 @@ package ru.job4j.coverter;
 
 public class Converter {
     public static int rubleToEuro(int value) {
-        int rs1 = value / 70;
-                return rs1;
+        return value / 70;
     }
+
     public static int rubletoDollar(int value) {
-        int rs1 = value / 60;
-                return rs1;
+        return value / 60;
     }
+
     public static void main(String[] args) {
         int euro = Converter.rubleToEuro(280);
-                System.out.println("280 rubles "+ euro + " euro");
+                int in = 280;
+                int expected = 4;
+                int out = Converter.rubleToEuro(in);
+                boolean passed = expected == out;
+        System.out.println("280 rubles " + euro + " euro " + passed);
+
         int dollar = Converter.rubletoDollar(120);
-                System.out.println("120 rubles " + dollar + " dollar");
+                int in1 = 120;
+                int expected1 = 2;
+                int out1 = Converter.rubletoDollar(in);
+                boolean passed1 = expected == out;
+                System.out.println("120 rubles " + dollar + " dollar " + passed1);
     }
+
 }
