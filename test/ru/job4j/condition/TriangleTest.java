4bb971f61b5f3979fec9f1d94a1deccb107e533f
ListerioAxxe <langua095@gmail.com>
2021-01-13 20:07:32 +0300

    TriangeTest

---
+++ test/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+
+    @Test
+    public void whenExist() {   //метод со своим названием
+        boolean result = Triangle.exist(2.0, 2.0, 2.0);  // исходные данные , вызываем метод
+        boolean is = true; // данная с чем сравнить//
+        Assert.assertEquals(result, is); // результат теста
+
+    }
+}
