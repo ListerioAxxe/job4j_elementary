34a84af35bd12629b318531322019aebf4e79adc
ListerioAxxe <langua095@gmail.com>
2021-01-13 11:24:56 +0300

    Cinema1

--- src/ru/job4j/condition/Cinema1.java
+++ src/ru/job4j/condition/Cinema1.java
@@ -1,11 +1,19 @@
 package ru.job4j.condition;
 
+
 public class Cinema1 {
+
     public static void permission(boolean allowByParent, boolean hasMoney) {
-        if (allowByParent && hasMoney) System.out.println("Yes,you can");
-        else System.out.println("No,you cant");
+        if (allowByParent && hasMoney) {
+            System.out.println("Yes,you can");
+        } else {
+            System.out.println("No,you cant");
+        }
     }
-    public static void main(String[] args) {
-        permission(false, true);
-    }
+        public static void main (String[]args){
+            permission(false, true);
+
+        }
+
 }
+
