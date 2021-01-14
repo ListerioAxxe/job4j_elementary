7efca453211463dbb4f7d3c4567dd945409c96db
ListerioAxxe <langua095@gmail.com>
2021-01-14 21:26:52 +0300

    ChessBoard

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -9,7 +9,7 @@ public class TriangleTest {
 
     @Test
     public void whenExist() {   //метод со своим названием
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);  // исходные данные , вызываем метод
+        boolean result = Triangle.exist(2.0, 2.0, 3.0);  // исходные данные , вызываем метод
         boolean is = true; // данная с чем сравнить//
         Assert.assertEquals(result, is); // результат теста
 
