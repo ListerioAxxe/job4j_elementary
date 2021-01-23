6e2216a0cda7289d3a7ce24f0f7b4edf5820e838
ListerioAxxe <langua095@gmail.com>
2021-01-23 19:26:09 +0300

    ArrayDefinition

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,12 +1,18 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+import java.util.spi.CalendarNameProvider;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива ages " + ages.length);
-        System.out.println("Размер массива surnames " + surnames.length);
-        System.out.println("Размер массива prices " + prices.length);
+       String[] names = new String[4];
+       names[0] = "Evgen";
+       names[1] = "Vika";
+       names[2] = "Kazir";
+       names[3] = "Vasya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
