8b5f649954e9e2a0b06e0fae02fed651e9e59c41
ListerioAxxe <langua095@gmail.com>
2021-01-24 19:31:00 +0300

    TestSquare

---
+++ test/ru/job4j/array/SquareTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+
+    @Test
+    public void whenBound4Then0149() {
+        int bound = 4;
+        int[] rst = Square.calculate(bound);
+        int[] exp = new int[] {0, 1, 4, 9};
+        assertThat(rst, is(exp));
+
+
+    }
+}
