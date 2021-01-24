ca343ab17661a87769257ec3edce6c16f1b89173
ListerioAxxe <langua095@gmail.com>
2021-01-24 18:39:16 +0300

    Square

--- src/ru/job4j/array/ArrayLoop.java
+++ src/ru/job4j/array/ArrayLoop.java
@@ -1,7 +1,6 @@
 package ru.job4j.array;
 
-
-public class ArrayLoop {
+ public class ArrayLoop {
     public static void main(String[] args) {
         int[] elements = new int[5];
         for (int i = 0; i < elements.length; i++) {
