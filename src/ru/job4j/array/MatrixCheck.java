01af89d8e53eaf82a153607a7c469840fc3c8cbf
ListerioAxxe <langua095@gmail.com>
2021-04-02 19:04:25 +0300

    MatrixCheck

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -12,4 +12,15 @@ public class MatrixCheck {
             }
             return result;
         }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result1 = true;
+        for (int i = 0; i < board.length; i++) {
+            if ('X' != board[i][column]) {
+                result1 = false;
+                break;
+            }
+        }
+        return result1;
+    }
     }
