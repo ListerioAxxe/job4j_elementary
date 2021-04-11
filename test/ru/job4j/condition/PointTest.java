ea177cf28b3966dbe361b1cc9126b5aad63d755a
ListerioAxxe <langua095@gmail.com>
2021-04-11 15:44:03 +0300

    PointTest

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -1,39 +1,23 @@
 package ru.job4j.condition;
 
+
+
 import org.junit.Assert;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PointTest {
 
     @Test
-    public void When00to20then2() {
-        int x1 = 0;
-        int x2 = 2;
-        int y1 = 0;
-        int y2 = 0;
-        int exp = 2;
-        double rsl = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(exp, rsl, 0.01);
+    public void pointTest1() {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 2);
+        Point rsl = new Point(first, second);
+        double dist = first.distance(second);
+        assertThat(dist, rsl);
+
     }
-    @Test
-    public void When11to11then0 () {
-        int x1 = 1;
-        int x2 = 1;
-        int y1 = 1;
-        int y2 = 1;
-        int exp = 0;
-        double rsl = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(rsl, exp,0.01);
+
     }
-    @Test
-    public void When12to12then1 () {
-        int x1 = 1;
-        int x2 = 2;
-        int y1 = 1;
-        int y2 = 2;
-        double exp = 1.4;
-        double rsl = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(rsl, exp,0.02);
-    }
-}
